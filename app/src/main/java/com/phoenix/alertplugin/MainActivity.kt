1bfa469afc50cdb7d21ba445917f856d63aae1a9
rasel <raselk@itmedicus.com>
2020-09-19 14:29:31 +0600

    added delete popup

--- app/src/main/java/com/phoenix/alertplugin/MainActivity.kt
+++ app/src/main/java/com/phoenix/alertplugin/MainActivity.kt
@@ -2,10 +2,15 @@ package com.phoenix.alertplugin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.muslimit.boom_boom_alert.BoomBoomAlert
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            BoomBoomAlert(this).DeleteAlert(message="Are you sure want to delete?")
+        }
     }
 }
